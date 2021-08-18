3dc88a4885c05874aaee8b4288052ec2c2ad8116
thrlstl <matthewsteeleonline@gmail.com>
2021-08-18 11:02:50 -0500

    Resolving 404 Error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::API
     
     def decoded_token
         if auth_header
-            @token = auth_header.split(' ')[1]
+            token = auth_header.split(' ')[1]
             begin
-                JWT.decode(@token, Rails.application.secret_key_base[0], true, algorithm: 'HS256')
+                JWT.decode(token, Rails.application.secret_key_base[0], true, algorithm: 'HS256')
             rescue JWT::DecodeError
                 nil
             end
