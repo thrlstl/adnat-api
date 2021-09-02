32e86b33eaf671165819a35cfc63b6ce9b81513c
thrlstl <matthewsteeleonline@gmail.com>
2021-09-02 16:35:33 -0500

    Removing Name Length validation for db seed purposes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,5 @@
 class Organization < ApplicationRecord
     validates :name, :hourly_rate, presence: true
-    validates_length_of :name, minimum: 5, maximum: 15, allow_blank: true
     validates :hourly_rate, numericality: { greater_than_or_equal_to: 11 }, allow_blank: true
     has_many :organization_users
     has_many :users, through: :organization_users, dependent: :destroy
